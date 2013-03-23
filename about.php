1a02f2e16eae00626c87c0dd2cf24bd1e953f3b7
lokesh <yokesh.ganesan@gmail.com>
2013-03-23 15:25:47 +0530

    Signed-off-by: lokesh <yokesh.ganesan@gmail.com>

--- about.php
+++ about.php
@@ -48,55 +48,7 @@
 
 
 	<!--==============================header=================================-->
-	<header>
-		<div class="container">
-			<div class="navbar navbar_ clearfix">
-				<div class="navbar-inner">
-					<div class="clearfix">
-						<h1 class="brand">
-							<a href="index.php"> <img src="images/logo.png" alt=""></a>
-						</h1>
-						<div class="nav-collapse nav-collapse_ collapse row-fluid ">
-							<div class="clearfix">
-								<ul class="nav sf-menu clearfix sf-js-enabled ">
-									<li class=""><a href="index.php">Home</a></li>
-									<li class="active"><a href="about.php">About Us</a></li>
-									<li><a href="#">Product</a></li>
-									<li><a href="service.php">Services</a></li>
-									<li><a href="#">Carriers</a></li>
-									<li><a href="contact.php">Contact Us</a></li>
-								</ul>
-								<select class="select-menu" style="display: inline-block;"><option
-										value="#">Navigate to...</option>
-									<option value="#" selected="selected">Home</option>
-									<option value="#">Company</option>
-									<option value="#">––Analyst relations</option>
-									<option value="#">––Awards</option>
-									<option value="#">–––Green enterprise</option>
-									<option value="#">–––Legal Information</option>
-									<option value="#">–––Management team</option>
-									<option value="#">–––Press &amp; Media</option>
-									<option value="#">––Company information</option>
-									<option value="#">––Consultants</option>
-									<option value="#">––Events</option>
-									<option value="#">Products &amp; Services</option>
-									<option value="#">Support</option>
-									<option value="#">Contacts</option></select>
-								<div class="div-search">
-									<span>search</span>
-									<form id="search" action="#" method="GET"
-										accept-charset="utf-8">
-										<input type="text" value="" name="s"> <a href="#"
-											onclick="document.getElementById(&#39;search&#39;).submit()"></a>
-									</form>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</header>
+	<?php include("header.php")?>
 
 	<div id="content">
 		<div class="container">
--- contact.php
+++ contact.php
@@ -38,51 +38,7 @@
 
 <body>
 	<!--==============================header=================================-->
-	<header>
-		<div class="container">
-			<div class="navbar navbar_ clearfix">
-				<div class="navbar-inner">
-					<div class="clearfix">
-						<h1 class="brand">
-							<a href="index.php"> <img src="images/logo.png" alt="">
-							</a>
-						</h1>
-						<div class="nav-collapse nav-collapse_ collapse row-fluid ">
-							<div class="clearfix">
-								<ul class="nav sf-menu clearfix sf-js-enabled ">
-									<li><a href="index.php">Home</a></li>
-									<li class="sub-menu">
-									<a href="about.php">About Us</a>
-									</li>
-									<li><a href="#">Product</a></li>
-									<li><a href="service.php">Services</a></li>
-									<li><a href="#">Carriers</a></li>
-									<li class="active"><a href="contact.php">Contact Us</a></li>
-								</ul>
-								<select class="select-menu" style="display: inline-block;"><option
-										value="#">Navigate to...</option>
-									<option value="#">Home</option>
-									<option value="#">About Us</option>
-									<option value="#">Product</option>
-									<option value="#">Services</option>
-									<option value="#">Carriers</option>
-									<option value="#">Contact Us</option>
-								</select>
-								<div class="div-search">
-									<span>search</span>
-									<form id="search" action="#" method="GET"
-										accept-charset="utf-8">
-										<input type="text" value="" name="s"> <a href="#"
-											onclick="document.getElementById('search').submit()"></a>
-									</form>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</header>
+	<?php include("header.php")?>
 
 	<div id="content-1">
 		<div class="container">

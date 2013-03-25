9db115d36836d5c467b7b92ab5a02abb965bfb81
lokesh <yokesh.ganesan@gmail.com>
2013-03-25 14:11:15 +0530

    Signed-off-by: lokesh <yokesh.ganesan@gmail.com>

--- protected/modules/front/views/site/product.php
+++ protected/modules/front/views/site/product.php
@@ -2,10 +2,13 @@
 	<?php include 'header.php'; ?>
 	<div id="content">
 		<div class="container">
-			<div class="row"></div>
-
-
-
-
+			<div class="row-fluid">
+			
+			<div class="span3">
+			<h3>Business Cards</h3>
+			</div>
+			
+			
+			</div>
 		</div>
 	</div>

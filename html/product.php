23e0547e72f0f2849518579da9a746ea855d3899
janani <janu.vu@gmail.com>
2013-03-25 14:59:46 +0530

    Signed-off-by: janani <janu.vu@gmail.com>

--- html/product.php
+++ html/product.php
@@ -1,49 +1,5 @@
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>product</title>
-<meta charset="utf-8">
-<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
-<link rel="icon" href="img/favicon.ico" type="image/x-icon">
-<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
-<meta name="description" content="Your description">
-<meta name="keywords" content="Your keywords">
-<meta name="author" content="Your name">
-<meta name="viewport"
-		content="width=device-width,initial-scale=1.0,user-scalable=0">
-<meta name="format-detection" content="telephone=no" />
-<link rel="stylesheet" href="css/bootstrap.css" type="text/css"
-	media="screen">
-<link rel="stylesheet" href="css/responsive.css" type="text/css"
-	media="screen">
-<link rel="stylesheet" href="css/style.css" type="text/css"
-	media="screen">
-<script type="text/javascript"
-	src="http://code.jquery.com/jquery-1.9.1.js"></script>
-<script type="text/javascript" src="js/superfish.js"></script>
-<script type="text/javascript" src="js/jquery.mobilemenu.js"></script>
-<script type="text/javascript" src="js/jquery.cookie.js"></script>
-
-
-
-<!--[if lt IE 8]>
-    		<div style='text-align:center'><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/img/upgrade.jpg"border="0"alt=""/></a></div>  
-   	<![endif]-->
-<!--[if lt IE 9]>
-      <link rel="stylesheet" href="css/docs.css" type="text/css" media="screen">
-      <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-</head>
-
-<body>
-	<!--==============================header=================================-->
-	<?php include("header.php");
-	echo headerContant('product');
-	
-	?>
 
 	
 	
@@ -77,29 +33,3 @@
 	
 	
 	
-	<?php require('footer.php')?>
-
-
-			<script type="text/javascript" src="js/bootstrap.js"></script>
-	
-
-	<!--LIVEDEMO_00 -->
-
-	<script type="text/javascript">
- var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-7078796-5']);
-  _gaq.push(['_trackPageview']);
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();</script>
-
-
-	<div id="shadowMeasureIt"></div>
-	<div id="divCoordMeasureIt"></div>
-	<div id="divRectangleMeasureIt">
-		<div id="divRectangleBGMeasureIt"></div>
-	</div>
-</body>
-</html>

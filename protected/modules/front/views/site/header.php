5f88669f065e1dcf4b8450e0f9b3a9cca50b3dc8
lokesh <yokesh.ganesan@gmail.com>
2013-03-28 10:50:44 +0530

    Signed-off-by: lokesh <yokesh.ganesan@gmail.com>

--- protected/modules/front/views/site/header.php
+++ protected/modules/front/views/site/header.php
@@ -1,38 +1,56 @@
-	<header>
-		<div class="container">
-			<div class="navbar navbar_ clearfix">
-				<div class="navbar-inner">
-					<div class="clearfix">
-						<h1 class="brand">
-							<a href="<a href="<?php echo Yii::app()->CreateAbsoluteUrl('/home')?>"> <img src="<?php echo Yii::app()->theme->baseUrl;?>/images/logo.png" alt="">
-							</a>
-						</h1>
-						<div class="nav-collapse nav-collapse_ collapse row-fluid ">
-							<div class="clearfix">
-								<ul class="nav sf-menu clearfix sf-js-enabled  ">
-									<li <?php echo (Yii::app()->controller->action->id== 'index' ? 'class=active' : '');?>><a href="<?php echo Yii::app()->CreateAbsoluteUrl('/home')?>">Home</a></li>
-									<li <?php echo (Yii::app()->controller->action->id== 'about' ? 'class=active' : '');?>>
-									<a href="<?php echo Yii::app()->CreateAbsoluteUrl('/about')?>">About Us</a>
-									</li>
-									<li <?php echo (Yii::app()->controller->action->id== 'product' ? 'class=active' : '');?>><a href="<?php echo Yii::app()->CreateAbsoluteUrl('/product')?>">Product</a></li>
-									<li <?php echo (Yii::app()->controller->action->id== 'service' ? 'class=active' : '');?>><a href="<?php echo Yii::app()->CreateAbsoluteUrl('/service')?>">Services</a></li>
-									<li <?php echo (Yii::app()->controller->action->id== 'career' ? 'class=active' : '');?>><a href="<?php echo Yii::app()->CreateAbsoluteUrl('/career')?>">Career</a></li>
-									<li <?php echo (Yii::app()->controller->action->id== 'contact' ? 'class=active' : '');?>><a href="<?php echo Yii::app()->CreateAbsoluteUrl('/contact')?>">Contact Us</a></li>
-							
-								</ul>
-								
-								<div class="div-search">
-									<span>search</span>
-									<form id="search" action="#" method="GET"
-										accept-charset="utf-8">
-										<input type="text" value="" name="s"> <a href="#"
-											onclick="document.getElementById('search').submit()"></a>
-									</form>
-								</div>
+
+<header>
+
+	
+	
+	<div class="container">
+
+		<div class="navbar navbar_ clearfix">
+			<div class="navbar-inner">
+				<div class="clearfix">
+					<h1 class="brand"></h1>
+					<div class="nav-collapse nav-collapse_ collapse row-fluid ">
+						<div class="clearfix">
+							<ul class="nav sf-menu clearfix sf-js-enabled  ">
+								<li
+								<?php echo (Yii::app()->controller->action->id== 'index' ? 'class=active' : '');?>><a
+									href="<?php echo Yii::app()->CreateAbsoluteUrl('/home')?>">Home</a>
+								</li>
+								<li
+								<?php echo (Yii::app()->controller->action->id== 'about' ? 'class=active' : '');?>>
+									<a href="<?php echo Yii::app()->CreateAbsoluteUrl('/about')?>">About
+										Us</a>
+								</li>
+								<li
+								<?php echo (Yii::app()->controller->action->id== 'product' ? 'class=active' : '');?>><a
+									href="<?php echo Yii::app()->CreateAbsoluteUrl('/product')?>">Product</a>
+								</li>
+								<li
+								<?php echo (Yii::app()->controller->action->id== 'service' ? 'class=active' : '');?>><a
+									href="<?php echo Yii::app()->CreateAbsoluteUrl('/service')?>">Services</a>
+								</li>
+								<li
+								<?php echo (Yii::app()->controller->action->id== 'career' ? 'class=active' : '');?>><a
+									href="<?php echo Yii::app()->CreateAbsoluteUrl('/career')?>">Career</a>
+								</li>
+								<li
+								<?php echo (Yii::app()->controller->action->id== 'contact' ? 'class=active' : '');?>><a
+									href="<?php echo Yii::app()->CreateAbsoluteUrl('/contact')?>">Contact
+										Us</a></li>
+
+							</ul>
+
+							<div class="div-search">
+								<span>search</span>
+								<form id="search" action="#" method="GET" accept-charset="utf-8">
+									<input type="text" value="" name="s"> <a href="#"
+										onclick="document.getElementById('search').submit()"></a>
+								</form>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</header>
+	</div>
+</header>
--- protected/modules/front/views/site/index.php
+++ protected/modules/front/views/site/index.php
@@ -1,3 +1,16 @@
-<body id="splash" cz-shortcut-listen="true">
-<!--==============================header=================================-->
-<?php include 'header.php'; ?>
+
+<body
+	id="splash" cz-shortcut-listen="true">
+	<div id="homepage" class="row-fluid">
+		<div class="logo span3">
+			<a href="#"><img
+				src="<?php echo Yii::app()->theme->baseUrl;?>/images/logo.png"> </a>
+		</div>
+
+		<div class="top span12"></div>
+
+	</div>
+
+
+	<!--==============================header=================================-->
+	<?php include 'header.php'; ?>

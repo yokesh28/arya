66705ad1949b34050fda686a802564c2788342d3
lokesh <yokesh.ganesan@gmail.com>
2013-04-18 12:10:07 +0530

    Signed-off-by: lokesh <yokesh.ganesan@gmail.com>

--- protected/modules/front/views/site/list.php
+++ protected/modules/front/views/site/list.php
@@ -1,464 +1,451 @@
 
-        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl;?>/css/demo.css" />
-        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl;?>/css/style_common.css" />
-        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl;?>/css/style3.css" />
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700,300,300italic' rel='stylesheet' type='text/css' />
-		<script type="text/javascript" src="<?php echo Yii::app()->theme->baseUrl;?>/js/modernizr.custom.69142.js"></script> 
+<link
+	rel="stylesheet" type="text/css"
+	href="<?php echo Yii::app()->theme->baseUrl;?>/css/demo.css" />
+<link
+	rel="stylesheet" type="text/css"
+	href="<?php echo Yii::app()->theme->baseUrl;?>/css/style_common.css" />
+<link
+	rel="stylesheet" type="text/css"
+	href="<?php echo Yii::app()->theme->baseUrl;?>/css/style3.css" />
+<link
+	href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700,300,300italic'
+	rel='stylesheet' type='text/css' />
+<script
+	type="text/javascript"
+	src="<?php echo Yii::app()->theme->baseUrl;?>/js/modernizr.custom.69142.js"></script>
 <body cz-shortcut-listen="true">
-<div id="innerpage">
+	<div id="innerpage">
 
-	<?php include 'header.php'; ?>
-	<div id="content">
-		<div class="container imgsize">
-		<h2>Visiting Card</h2>
-			<div class="row rowsize">
-			<form id="paypal_form"  action="payments.php" method="post" >
-			<div class="product span6">
-
-				
-				<div id="grid" class="main list ">
-
-            
-            <h5>Gloss</h5>
-				<div class="view">
-					<div class="view-back">
-						
-						
-					</div>
-						<img class="imgsize" src="<?php echo Yii::app()->baseUrl?>/upload/product/gloss/1.jpg">
-					
-					
-					
-					
-					<div class="price">
-					<div class="quantityinfo">
-						<label><b>Quantity:</b></label> <select name="quantity"><option>500nos</option>
-						<option>1000nos</option>
-						
-						</select><br><br>
-						<label><b>Type :</b></label> <select name="type"><option>Single Side</option>
-						<option>Front&Back</option>
-						
-						</select>
-					
-			</div>
-					
-					
-					</div>
-					
-					
-					
-					</div>
-			</div>
-				<div class="isize">
-		<input type="image" src="<?php echo Yii::app()->theme->baseUrl;?>/images/buy.png" alt="submit">
-			</div>	
-		</div>
-		
-		</form>
-	
-				<div class="product span6">
-				
-				
-			
-
-            <div id="grid1" class="main list ">
-            <h5>Matt</h5>
-				<div class="view">
-					<div class="view-back">
-						
-						
-					</div>
-						<img class="imgsize" src="<?php echo Yii::app()->baseUrl?>/upload/product/mat/1.jpeg">
-					
-					
-					
-					
-					<div class="price">
-						<label><b>Quantity:</b></label> <select name="quantity1"><option>500nos</option>
-						<option>1000nos</option>
-						
-						</select><br><br>
-						<label><b>Type :</b></label> <select name="type1"><option>Single Side</option>
-						<option>Front&Back</option>
-						
-						</select>
-					
-					
-					
-					</div>
-					
-					
-					</div>
-			</div>
-				<div class="isize">
-		<input type="image" src="<?php echo Yii::app()->theme->baseUrl;?>/images/buy.png" alt="submit">
-			</div>		
-			
-		</div>
-		
-		<div class="product span6">
-				
-				
-			
-
-            <div id="grid2" class="main list ">
-           <h5>Synthetic</h5>
-				<div class="view">
-					<div class="view-back">
-						
-						
-					</div>
-						<img class="imgsize" src="<?php echo Yii::app()->baseUrl?>/upload/product/sy/1.jpg">
-					
-					
-					
-					
-					<div class="price">
-						<label><b>Quantity:</b></label> <select name="quantity2"><option>500nos</option>
-						<option>1000nos</option>
-						
-						</select><br><br>
-						<label><b>Type :</b></label> <select name="type2"><option>Single Side</option>
-						<option>Front&Back</option>
-						
-						</select>
-					
-					
-					</div>
-					
-					
-					</div>
-			</div>
-			<div class="isize">
-		<input type="image" src="<?php echo Yii::app()->theme->baseUrl;?>/images/buy.png" alt="submit">
-			</div>	
-			
-		</div>
-		
-		<div class="product span6">
-				
-				
-			
-
-            <div id="grid3" class="main list ">
-           <h5>Uv</h5>	
-				<div class="view">
-					<div class="view-back">
-						
-						
-					</div>
-						<img class="imgsize" src="<?php echo Yii::app()->baseUrl?>/upload/product/uv/1.jpg">
-					
-					
-					
-					
-					<div class="price">
-						<label><b>Quantity:</b></label> <select name="quantity3"><option>500nos</option>
-						<option>1000nos</option>
-						
-						</select><br><br>
-						<label><b>Type :</b></label> <select name="type3"><option>Per Side</option>
-						
-						
-						</select>
-					
-					
-					
-					</div>
-					
-					
-					</div>
-			</div>
-				<div class="isize">
-		<input type="image" src="<?php echo Yii::app()->theme->baseUrl;?>/images/buy.png" alt="submit">
-			</div>	
-			
-		</div>
-		</div>
-		</div>
+		<?php include 'header.php'; ?>
 		<div id="content">
-		<div class="container imgsize">
-		<h2>Letter Heads</h2>
-			<div class="row rowsize">
-		
-		<div class="product span6">
-				
-				
-			
+			<div class="container imgsize">
+				<h2>Visiting Card</h2>
+				<div class="row rowsize">
+					<form id="paypal_form" action="payments.php" method="post">
+						<div class="product span6">
 
-            <div id="grid4" class="main list ">
-           <h5>Letter Head</h5>	
-				<div class="view">
-					<div class="view-back">
-						
-						
+
+							<div id="grid" class="main list ">
+
+
+								<h5>Gloss</h5>
+								<div class="view">
+									<div class="view-back"></div>
+									<img class="imgsize"
+										src="<?php echo Yii::app()->baseUrl?>/upload/product/gloss/1.jpg">
+									<div class="price">
+										<div class="quantityinfo">
+											<label><b>Quantity:</b> </label> <select name="quantity"><option>500nos</option>
+												<option>1000nos</option>
+											</select><br> <br> <label><b>Type :</b> </label> <select
+												name="type"><option>Single Side</option>
+												<option>Front&Back</option>
+											</select>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="isize">
+								<input type="image"
+									src="<?php echo Yii::app()->theme->baseUrl;?>/images/buy.png"
+									alt="submit">
+							</div>
+						</div>
+
+					</form>
+
+					<div class="product span6">
+						<div id="grid1" class="main list ">
+							<h5>Matt</h5>
+							<div class="view">
+								<div class="view-back"></div>
+								<img class="imgsize"
+									src="<?php echo Yii::app()->baseUrl?>/upload/product/mat/1.jpeg">
+								<div class="price">
+									<label><b>Quantity:</b> </label> <select name="quantity1"><option>500nos</option>
+										<option>1000nos</option>
+
+									</select><br> <br> <label><b>Type :</b> </label> <select
+										name="type1"><option>Single Side</option>
+										<option>Front&Back</option>
+
+									</select>
+								</div>
+
+
+							</div>
+						</div>
+						<div class="isize">
+							<input type="image"
+								src="<?php echo Yii::app()->theme->baseUrl;?>/images/buy.png"
+								alt="submit">
+						</div>
+
 					</div>
-						<img class="imgsize" src="<?php echo Yii::app()->baseUrl?>/upload/product/letterhead/1.jpg">
-					
-					
-					
-					
-					<div class="price">
-						<label><b>Quantity:</b></label> <select name="quantity4"><option>500nos</option>
-						<option>1000nos</option>
-						
-						</select><br><br>
-						<label><b>Type :</b></label> <select name="type4"><option>Single Side</option>
-						<option>extra</option>
-						<option>Per Side</option>
-						<option>Knurling</option>
-						<option>envelope</option>
-					
-						</select>
-					
-					
-					
-					
+
+					<div class="product span6">
+
+
+
+
+						<div id="grid2" class="main list ">
+							<h5>Synthetic</h5>
+							<div class="view">
+								<div class="view-back"></div>
+								<img class="imgsize"
+									src="<?php echo Yii::app()->baseUrl?>/upload/product/sy/1.jpg">
+
+
+
+
+								<div class="price">
+									<label><b>Quantity:</b> </label> <select name="quantity2"><option>500nos</option>
+										<option>1000nos</option>
+
+									</select><br> <br> <label><b>Type :</b> </label> <select
+										name="type2"><option>Single Side</option>
+										<option>Front&Back</option>
+
+									</select>
+
+
+								</div>
+
+
+							</div>
+						</div>
+						<div class="isize">
+							<input type="image"
+								src="<?php echo Yii::app()->theme->baseUrl;?>/images/buy.png"
+								alt="submit">
+						</div>
+
 					</div>
-					
-					
+
+					<div class="product span6">
+
+
+
+
+						<div id="grid3" class="main list ">
+							<h5>Uv</h5>
+							<div class="view">
+								<div class="view-back"></div>
+								<img class="imgsize"
+									src="<?php echo Yii::app()->baseUrl?>/upload/product/uv/1.jpg">
+
+
+
+
+								<div class="price">
+									<label><b>Quantity:</b> </label> <select name="quantity3"><option>500nos</option>
+										<option>1000nos</option>
+
+									</select><br> <br> <label><b>Type :</b> </label> <select
+										name="type3"><option>Per Side</option>
+
+
+									</select>
+
+
+
+								</div>
+
+
+							</div>
+						</div>
+						<div class="isize">
+							<input type="image"
+								src="<?php echo Yii::app()->theme->baseUrl;?>/images/buy.png"
+								alt="submit">
+						</div>
+
 					</div>
+				</div>
 			</div>
-				<div class="isize">
-		<input type="image" src="<?php echo Yii::app()->theme->baseUrl;?>/images/buy.png" alt="submit">
-			</div>	
-			
-		</div>
-		
-		
-		
-		<div class="product span6">
-				
-				
-			
+			<div id="content">
+				<div class="container imgsize">
+					<h2>Letter Heads</h2>
+					<div class="row rowsize">
 
-            <div id="grid5" class="main list ">
-            <h5>Pad Charges</h5>	
-				<div class="view">
-					<div class="view-back">
-						
-						
+						<div class="product span6">
+
+
+
+
+							<div id="grid4" class="main list ">
+								<h5>Letter Head</h5>
+								<div class="view">
+									<div class="view-back"></div>
+									<img class="imgsize"
+										src="<?php echo Yii::app()->baseUrl?>/upload/product/letterhead/1.jpg">
+
+
+
+
+									<div class="price">
+										<label><b>Quantity:</b> </label> <select name="quantity4"><option>500nos</option>
+											<option>1000nos</option>
+
+										</select><br> <br> <label><b>Type :</b> </label> <select
+											name="type4"><option>Single Side</option>
+											<option>extra</option>
+											<option>Per Side</option>
+											<option>Knurling</option>
+											<option>envelope</option>
+
+										</select>
+
+
+
+
+									</div>
+
+
+								</div>
+							</div>
+							<div class="isize">
+								<input type="image"
+									src="<?php echo Yii::app()->theme->baseUrl;?>/images/buy.png"
+									alt="submit">
+							</div>
+
+						</div>
+
+
+
+						<div class="product span6">
+
+
+
+
+							<div id="grid5" class="main list ">
+								<h5>Pad Charges</h5>
+								<div class="view">
+									<div class="view-back"></div>
+									<img class="imgsize"
+										src="<?php echo Yii::app()->baseUrl?>/upload/product/pad/1.jpg">
+
+
+
+
+									<div class="price">
+										<label><b>Quantity:</b> </label> <select name="quantity5"><option>10pads</option>
+
+
+										</select><br> <br> <label><b>Type :</b> </label> <select
+											name="type5"><option>Single Side</option>
+											<option>extra</option>
+											<option>Per Side</option>
+											<option>Knurling</option>
+											<option>envelope</option>
+
+										</select>
+
+
+
+
+									</div>
+
+
+								</div>
+							</div>
+							<div class="isize">
+								<input type="image"
+									src="<?php echo Yii::app()->theme->baseUrl;?>/images/buy.png"
+									alt="submit">
+							</div>
+
+						</div>
+
+
+
 					</div>
-						<img class="imgsize" src="<?php echo Yii::app()->baseUrl?>/upload/product/pad/1.jpg">
-					
-					
-					
-					
-					<div class="price">
-						<label><b>Quantity:</b></label> <select name="quantity5"><option>10pads</option>
-						
-						
-						</select><br><br>
-						<label><b>Type :</b></label> <select name="type5"><option>Single Side</option>
-						<option>extra</option>
-						<option>Per Side</option>
-						<option>Knurling</option>
-						<option>envelope</option>
-						
-						</select>
-					
-					
-					
-					
-					</div>
-					
-					
-					</div>
+				</div>
 			</div>
-				<div class="isize">
-		<input type="image" src="<?php echo Yii::app()->theme->baseUrl;?>/images/buy.png" alt="submit">
-			</div>		
-			
-		</div>
-		
-		
-		
-		</div>
-		</div>
-		</div>
-		<div id="content">
-		<div class="container imgsize">
-		<h2>Sticker</h2>
-			<div class="row rowsize">
-		<div class="product span6">
-				
-				
-			
+			<div id="content">
+				<div class="container imgsize">
+					<h2>Sticker</h2>
+					<div class="row rowsize">
+						<div class="product span6">
 
-            <div id="grid6" class="main list ">
-            <h5>Sticker</h5>	
-				<div class="view">
-					<div class="view-back">
-						
-						
+
+
+
+							<div id="grid6" class="main list ">
+								<h5>Sticker</h5>
+								<div class="view">
+									<div class="view-back"></div>
+									<img class="imgsize"
+										src="<?php echo Yii::app()->baseUrl?>/upload/product/sticker/1.jpg">
+
+
+
+
+									<div class="price">
+										<label><b>Quantity:</b> </label> <select name="quantity6">
+											<option>1000nos</option>
+
+										</select><br> <br> <label><b>Type :</b> </label> <select
+											name="type6"><option>5x5</option>
+
+											<option>1000nos</option>
+											<option>A4 Size</option>
+										</select>
+
+
+
+
+									</div>
+
+
+								</div>
+							</div>
+							<div class="isize">
+								<input type="image"
+									src="<?php echo Yii::app()->theme->baseUrl;?>/images/buy.png"
+									alt="submit">
+							</div>
+
+						</div>
+
+						<div class="product span6">
+
+
+
+
+							<div id="grid7" class="main list ">
+								<h5>CD Sticker</h5>
+								<div class="view">
+									<div class="view-back"></div>
+									<img class="imgsize"
+										src="<?php echo Yii::app()->baseUrl?>/upload/product/cd/1.jpg">
+
+
+
+
+									<div class="price">
+										<label><b>Quantity:</b> </label> <select name="quantity7">
+											<option>1000nos</option>
+
+										</select><br> <br> <label><b>Type :</b> </label> <select
+											name="type7"><option>5x5</option>
+
+											<option>1000nos</option>
+											<option>A4 Size</option>
+										</select>
+
+
+
+
+									</div>
+
+
+								</div>
+							</div>
+
+							<div class="isize">
+								<input type="image"
+									src="<?php echo Yii::app()->theme->baseUrl;?>/images/buy.png"
+									alt="submit">
+							</div>
+						</div>
+
+
 					</div>
-						<img class="imgsize" src="<?php echo Yii::app()->baseUrl?>/upload/product/sticker/1.jpg">
-					
-					
-					
-					
-					<div class="price">
-						<label><b>Quantity:</b></label> <select name="quantity6">
-						<option>1000nos</option>
-						
-						</select><br><br>
-						<label><b>Type :</b></label> <select name="type6"><option>5x5</option>
-						
-						<option>1000nos</option>
-						<option>A4 Size</option>
-						</select>
-					
-					
-					
-					
-					</div>
-					
-					
-					</div>
+				</div>
 			</div>
-				<div class="isize">
-		<input type="image" src="<?php echo Yii::app()->theme->baseUrl;?>/images/buy.png" alt="submit">
-			</div>	
-			
-		</div>
-			
-			<div class="product span6">
-				
-				
-			
+			<div id="content">
+				<div class="container imgsize">
+					<h2>Hand Outs</h2>
+					<div class="row rowsize">
 
-            <div id="grid7" class="main list ">
-            <h5>CD Sticker</h5>	
-				<div class="view">
-					<div class="view-back">
-						
-						
+						<div class="product span6">
+
+
+
+
+							<div id="grid8" class="main list ">
+
+								<div class="view">
+									<div class="view-back"></div>
+									<img class="imgsize"
+										src="<?php echo Yii::app()->baseUrl?>/upload/product/hand/1.jpg">
+
+
+
+
+									<div class="price">
+										<label><b>Quantity:</b> </label> <select name="quantity8">
+											<option>1000nos</option>
+
+										</select><br> <br> <label><b>Type :</b> </label> <select
+											name="type8"><option>SingleSide</option>
+											<option>front&back</option>
+
+										</select><br> <br> <label><b>Size :</b> </label><select
+											name="size"><option>170gsm - 210x280mm</option>
+											<option>100gsm - 210 x 280mm</option>
+											<option>170 Gsm - 140 x 210mm</option>
+											<option>100Gsm - 140 x 210mm</option>
+											<option>220 Gsm - 210 x 280mm</option>
+											<option>100gsm - 210 x 280mm</option>
+
+										</select>
+
+
+
+									</div>
+
+
+								</div>
+							</div>
+
+							<div class="isize hand">
+								<input type="image"
+									src="<?php echo Yii::app()->theme->baseUrl;?>/images/buy.png"
+									alt="submit">
+							</div>
+						</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 					</div>
-						<img class="imgsize" src="<?php echo Yii::app()->baseUrl?>/upload/product/cd/1.jpg">
-					
-					
-					
-					
-					<div class="price">
-						<label><b>Quantity:</b></label> <select name="quantity7">
-						<option>1000nos</option>
-						
-						</select><br><br>
-						<label><b>Type :</b></label> <select name="type7"><option>5x5</option>
-						
-						<option>1000nos</option>
-						<option>A4 Size</option>
-						</select>
-					
-					
-					
-					
-					</div>
-					
-					
-					</div>
+				</div>
+
+
+
+
+
+
 			</div>
-				
-			<div class="isize">
-		<input type="image" src="<?php echo Yii::app()->theme->baseUrl;?>/images/buy.png" alt="submit">
-			</div>		
-		</div>
-		
-		
-		</div>
-		</div>
-		</div>
-		<div id="content">
-		<div class="container imgsize">
-		<h2>Hand Outs</h2>
-			<div class="row rowsize">
-		
-		<div class="product span6">
-				
-				
-			
 
-            <div id="grid8" class="main list ">
-            
-				<div class="view">
-					<div class="view-back">
-						
-						
-					</div>
-						<img class="imgsize" src="<?php echo Yii::app()->baseUrl?>/upload/product/hand/1.jpg">
-					
-					
-					
-					
-					<div class="price">
-						<label><b>Quantity:</b></label> <select name="quantity8">
-						<option>1000nos</option>
-						
-						</select><br><br>
-						<label><b>Type :</b></label> <select name="type8"><option>SingleSide </option>
-						<option>front&back </option>
-						
-						</select><br><br>
-					<label><b>Size :</b></label><select name="size"><option>170gsm - 210x280mm </option>
-						<option>100gsm - 210 x 280mm </option>
-						<option>170 Gsm - 140 x 210mm </option>
-						<option>100Gsm - 140 x 210mm </option>
-						<option>220 Gsm - 210 x 280mm</option>
-						<option>100gsm - 210 x 280mm </option>
-						
-						</select>
-					
-					
-					
-					</div>
-					
-					
-					</div>
-			</div>
-					
-			<div class="isize hand">
-		<input type="image" src="<?php echo Yii::app()->theme->baseUrl;?>/images/buy.png" alt="submit">
-			</div>	
 		</div>
-		
-		
-			
-			
-		
-		
-		
-		
-		
-		
-		
 
-		
-		
-		
-	
-	
-	
-			
-			
-		</div>
-		</div>
-	
-	
-	
-	
-	
-	
+
+
 	</div>
-	
-	</div>
-		
-		
-		
-		</div>
-		
-	
-		<script type="text/javascript">	
+
+
+	<script type="text/javascript">	
 			
 			Modernizr.load({
 				test: Modernizr.csstransforms3d && Modernizr.csstransitions,
@@ -483,9 +470,3 @@
 			});
 				
 		</script>
-   
-	
-	
-	
-	
-	
